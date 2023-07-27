d0217965f06d46d82b9d23f26b3dfc8391fce011
sanjaytaylor2012 <sanjaytaylor2012@gmail.com>
2023-07-27 08:56:19 -0400

    adding backend

--- project3/src/backend/app.py
+++ project3/src/backend/app.py
@@ -1,2 +1,16 @@
 # to start the venv: $ source project3_env/Scripts/activate
-from flask import Flask
+# to start flask server: flask run
+# to start next.js: npm run dev
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route("/index", methods=["POST"])
+def index():
+    req = request.get_json()
+    response = {"message": "Data received successfully", "data": req}
+    return jsonify(response), 200
