26a90e30d2da88a12703ba9946f63546ae3a6437
sanjaytaylor2012 <sanjaytaylor2012@gmail.com>
2023-07-28 13:43:43 -0400

    doin big boy stuff

--- project3/src/backend/AdjList.py
+++ project3/src/backend/AdjList.py
@@ -1,19 +1,32 @@
 import Food
 
+
 class AdjList:
     def __init__(self):
+        # key: food + brand
+        # value: food obj, adj nodes
         self.graph = {}
-    
+
+    # def addEdge(self, _from, _to):
+    #     if self.graph.get((_from.name + _from.brand)) == None:
+    #         self.graph[(_from.name + _from.brand)][1] = []
+    #     self.graph[(_from.name + _from.brand)][0] = _from
+    #     self.graph[(_from.name + _from.brand)][1].append((_to.name + _to.brand))
+    #     if self.graph.get((_to.name + _to.brand)) == None:
+    #         self.graph[(_to.name + _to.brand)][0] = _to
+    #         self.graph[(_to.name + _to.brand)][1] = []
+
+    # def addVertex(self, food):
+    #     if self.graph.get((food.name + food.brand)) == None:
+    #         self.graph[(food.name + food.brand)][0] = food
+    #         self.graph[(food.name + food.brand)][1] = []
+
     def addEdge(self, _from, _to):
-        if(self.graph.get((_from.name + _from.brand)) == None):
-            self.graph[(_from.name + _from.brand)][1] = []
-        self.graph[(_from.name + _from.brand)][0] = _from
-        self.graph[(_from.name + _from.brand)][1].append((_to.name + _to.brand))
-        if(self.graph.get((_to.name + _to.brand)) == None):
-            self.graph[(_to.name + _to.brand)][0] = _to
-            self.graph[(_to.name + _to.brand)][1] = []
-    
+        self.graph[_from][1].append(_to)
+        self.graph[_to][1].append(_from)
+
     def addVertex(self, food):
-        if(self.graph.get((food.name + food.brand)) == None):
-            self.graph[(food.name + food.brand)][0] = food
-            self.graph[(food.name + food.brand)][1] = []
+        if (food.name) not in self.graph:
+            self.graph[(food.name)] = []
+            self.graph[(food.name)].append(food)
+            self.graph[(food.name)].append([])
--- project3/src/backend/app.py
+++ project3/src/backend/app.py
@@ -1,9 +1,12 @@
 # to start the venv: $ source project3_env/Scripts/activate
 # to start flask server: flask run
 # to start next.js: npm run dev
+# project3/src/backend/
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import csv
+from AdjList import AdjList
+from Food import Food
 
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
@@ -12,15 +15,8 @@ app = Flask(__name__)
 CORS(app)
 
 
-@app.route("/index", methods=["POST"])
-def index():
-    req = request.get_json()
-    print(req)
-    response = {"message": "Data received successfully", "data": req}
-    return jsonify(response), 200
-
 # function to return the cosine similarity of two strings
-def cosine_similarity(str1, str2):
+def cosine_sim(str1, str2):
     # Create a vectorizer
     vectorizer = TfidfVectorizer()
 
@@ -32,36 +28,64 @@ def cosine_similarity(str1, str2):
 
     return cosine_sim
 
-CONST_SIMILARITY_THRESHOLD = 0.55
-
-food_graph = AdjList()
 
 def parse_csv():
-    with open('backend/formatted_data.csv', mode='r', encoding="utf8") as file:
+    with open("formatted_data.csv", mode="r", encoding="utf8") as file:
         csvFile = csv.reader(file)
 
         first_line = True
+        count = 0
 
         for row in csvFile:
+            count += 1
+            if count > 10000:
+                break
             if first_line:
                 first_line = False
             else:
-                food_node = Food(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8])
-                
+                food_node = Food(
+                    row[0],
+                    row[1],
+                    # row[2],
+                    # row[3],
+                    # row[4],
+                    # row[5],
+                    # row[6],
+                    # row[7],
+                    # row[8],
+                )
+                print(food_node.name)
+
                 food_graph.addVertex(food_node)
+                count_nodes = 0
+                for key in food_graph.graph:
+                    if (
+                        key != food_node.name
+                        and cosine_sim(key, food_node.name) > CONST_SIMILARITY_THRESHOLD
+                    ):
+                        print("adding edge: from: ", food_node.name, " to: ", key)
+                        food_graph.addEdge(key, food_node.name)
+                        food_graph.addEdge(food_node.name, key)
+                        count_nodes += 1
+                    if count_nodes > 10:
+                        break
 
-                for key, value in food_graph.graph.items():
-                    if (cosine_similarity(key, food_node.name) > CONST_SIMILARITY_THRESHOLD):
-                        food_graph.addEdge(value[0], food_node)
-                        food_graph.addEdge(food_node, value[0])
+    count = 0
+    for key in food_graph.graph:
+        count += 1
+        print("Food: ", key, "adj nodes: ", print(food_graph.graph[key][1]))
+        if count > 10000:
+            break
 
 
+CONST_SIMILARITY_THRESHOLD = 0.2
+food_graph = AdjList()
+parse_csv()
 
 
-
-
-
-
-            
-
-        
+@app.route("/index", methods=["POST"])
+def index():
+    req = request.get_json()
+    print(req)
+    response = {"message": "Data received successfully", "data": req}
+    return jsonify(response), 200
