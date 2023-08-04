2b139964c7b9d18c4d4cf61226a8d88917fd3574
sanjaytaylor2012 <sanjaytaylor2012@gmail.com>
2023-08-04 15:16:14 -0400

    fixing csv

--- project3/src/backend/Food.py
+++ project3/src/backend/Food.py
@@ -1,6 +1,6 @@
 class Food:
     def __init__(
-        self, name, brand, url, servingSize, energy, fat, sugar, protein, fiber, sodium
+        self, name, brand, url, servingSize, energy, fat, sugar, fiber, protein, sodium
     ):
         self.name = name
         self.brand = brand
@@ -18,12 +18,21 @@ class Food:
         self.dailysodiumpercent = "N/A"
 
     def to_dict(self):
-        foodDict = {"name" : self.name, "brand": self.brand, "url" : self.url, 
-                    "serving_size" : self.servingSize, "energy" : self.energy,
-                    "fat" : self.fat, "sugar" : self.sugar, "protein" : self.protein,
-                    "fiber" : self.fiber, "sodium" : self.sodium, "dailyfatpercent" : self.dailyproteinpercent,
-                    "dailyproteinpercent" : self.dailyproteinpercent,"dailyfiberpercent" : self.dailyfiberpercent,
-                    "dailysodiumpercent" : self.dailysodiumpercent,}
+        foodDict = {
+            "name": self.name,
+            "brand": self.brand,
+            "url": self.url,
+            "serving_size": self.servingSize,
+            "energy": self.energy,
+            "fat": self.fat,
+            "sugar": self.sugar,
+            "protein": self.protein,
+            "fiber": self.fiber,
+            "sodium": self.sodium,
+            "dailyfatpercent": self.dailyfatpercent,
+            "dailyproteinpercent": self.dailyproteinpercent,
+            "dailyfiberpercent": self.dailyfiberpercent,
+            "dailysodiumpercent": self.dailysodiumpercent,
+        }
 
         return foodDict
-
